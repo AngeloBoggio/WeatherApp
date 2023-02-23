553289ce67292ffd92e48984e1e61e6af92292bc
Angelo <gelobogx@gmail.com>
2023-02-23 08:42:03 -0500

    corrected the magnifying glass animation/added a search bar image but need to fix the animation for the search bar

--- weather.js
+++ weather.js
@@ -1,24 +1,7 @@
 const myImage = document.getElementById('searchButton');
+const secondImage = document.getElementById('searchBar');
 
 myImage.addEventListener('click', () => {
-    const animation = myImage.animate(
-      [
-        // // start at the current position
-        { left: '200px' },
-        {top: '500px'},
-
-  
-        // end at 100px to the right
-        { right: '500px' }
-      ],
-      {
-        duration: 2000, // 500ms
-        easing: 'ease-out' // use ease-out timing function
-      }
-    );
-  
-    // attach transitionend event listener outside of animate()
-    myImage.addEventListener('transitionend', () => {
-      myImage.style.left = '200px'; // relocate the image to a new position
-    });
-  });
+  myImage.classList.add('animate');
+  secondImage.classList.add('animate');
+});
