bfb467094aa4d6e40964dbee14aee203133cbc86
Angelo <gelobogx@gmail.com>
2023-02-23 20:03:27 -0500

    Reached a good point with my animations and text box for user input, next steps will be storing user input and fetching data from API

--- weather.js
+++ weather.js
@@ -1,7 +1,13 @@
 const myImage = document.getElementById('searchButton');
 const secondImage = document.getElementById('searchBar');
+const textBox = document.getElementById('input-wrapper');
 
 myImage.addEventListener('click', () => {
-  myImage.classList.add('animate');
-  secondImage.classList.add('animate');
-});
+    myImage.classList.add('animate');
+    setTimeout(() => {
+      secondImage.style.display = 'block';
+      secondImage.classList.add('animate');
+      textBox.classList.add('animate');
+    }, 1000);
+  });
+
